e350b80eef9dd1355c0915fe3f1c41ec7b082e44
lucasgehin <lucasgehin88@gmail.com>
2014-01-23 13:12:43 +0100

    oi

--- themeController.php
+++ themeController.php
@@ -8,23 +8,28 @@ class themeController extends Controller{
 	}
 
 	public function listAction($t){
-		$res='<div class="row">
+		$res='<div class="row liste_theme">';
   		foreach (modele_theme::findAll() as $theme) {
-  		$res=$res.'<div class="thumbnail">
+  		$res=$res.'
+      
+    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
      	 <img data-src="images/'.$theme->__get('photo').'" alt="'.$theme->__get('photo').'">
       	<div class="caption">
         <h3>'.$theme->__get('nom').'</h3>
         <p>'.$theme->__get('description').'</p>
         <p><a href="theme.php?a=detail&theme='.$theme->__get('nom').'" class="btn btn-primary" role="button">Liste des restaurants</a></p>
      	 </div>
-    	</div>';
+    	</div>
+      </div>';
   		}
-   		$res=$res.'</div></div>';
+   		$res=$res.'</div>';
    		vue::affichage($res);
 	}
 
 	public function detailAction($t){
 		$res='<div class="row">
+ 		<div class="col-md-4">';
  		$theme = $_GET['theme'];
  		$nomTheme = modele_theme::findByNom($theme);
  		$idTheme = $nomTheme->__get('id');
