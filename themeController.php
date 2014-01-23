45f339d01f37c3387384865f4cfef20e870989f9
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-01-23 20:30:15 +0100

    change boutton quitter

--- themeController.php
+++ themeController.php
@@ -116,7 +116,7 @@ class themeController extends Controller{
               
               </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Quitter</button>
       </div>
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
