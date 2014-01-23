38600c72b6c183b674abf92f3efa574f085142fc
lucasgehin <lucasgehin88@gmail.com>
2014-01-23 13:16:36 +0100

    theme

--- themeController.php
+++ themeController.php
@@ -29,21 +29,24 @@ class themeController extends Controller{
 
 	public function detailAction($t){
 		$res='<div class="row">
- 		<div class="col-md-4">';
+ 		';
  		$theme = $_GET['theme'];
  		$nomTheme = modele_theme::findByNom($theme);
  		$idTheme = $nomTheme->__get('id');
   		foreach (modele_restaurant::findByIdtheme($idTheme) as $resto) {
-  		$res=$res.'<div class="thumbnail">
+  		$res=$res.'
+      <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
      	 <img data-src="images/'.$resto->__get('photo').'" alt="'.$resto->__get('photo').'">
       	<div class="caption">
         <h3>'.$resto->__get('nom').'</h3>
         <p>'.$resto->__get('description').'</p>
         <p><a href="plat.php?a=list&theme='.$theme.'&restaurant='.$resto->__get('nom').'&id='.$resto->__get("id"). '" class="btn btn-primary" role="button">Listes des plats</a> <a href="#" class="btn btn-default" role="button">Contact</a><a href="#" class="btn btn-default" role="button">Plan</a></p>
      	</div>
-    	</div>';
+    	</div>
+      </div>';
   		}
-   		$res=$res.'</div></div>';
+   		$res=$res.'</div>';
    		vue::affichage($res);
 	}
 
